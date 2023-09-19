40c159a34b3086b90d5f808119c4072aa5658fc0
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-19 04:26:58 -0300

    scripts/start_menu_hud.gd: reset

--- godot/scripts/start_menu_hud.gd
+++ godot/scripts/start_menu_hud.gd
@@ -1,14 +1,24 @@
 extends CanvasLayer
 
-## User-provided grid width
+## User-provided grid width (in cells)
 var user_width: int = 0
-## User-provided grid height
+## User-provided grid height (in cells)
 var user_height: int = 0
 ## User-provided mine count
 var user_mines: int = 0
 
+## Sets max mines to width * height
 func update_max_mines():
 	$ConfigContainer/MinesInput.max_value = user_width * user_height
+
+## Resets the configuration values
+func reset():
+	$ConfigContainer/WidthInput.value = 0.0
+	$ConfigContainer/HeightInput.value = 0.0
+	$ConfigContainer/MinesInput.value = 0.0
+	user_width = 0
+	user_height = 0
+	user_mines = 0
 
 func _on_width_input_value_changed(value: float) -> void:
 	user_width = maxi(value as int, 0)
