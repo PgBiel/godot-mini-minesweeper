9e3491ad9fda470382ff1bcdef69ddf0a7aa94ef
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-19 04:52:09 -0300

    main: initial progress (game at least starts with given settings)

---
+++ godot/scripts/main.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@onready var tile_map := $TileMapContainer/TileMapParent/TileMap
+
+func start():
+	$StartMenuHUD.hide()
+	var width: int = $StartMenuHUD.user_width
+	var height: int = $StartMenuHUD.user_height
+	var mines: int = $StartMenuHUD.user_mines
+	tile_map.init_board(width, height, mines)
+	$TileMapContainer.show()
+	print("starting game")
+
+func _on_start_menu_hud_start_button_clicked() -> void:
+	start()
+
+func _on_tile_map_bomb_revealed() -> void:
+	tile_map.reveal_all_bombs()
+	print("whoops")
