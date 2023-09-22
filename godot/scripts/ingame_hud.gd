24bb8510a86ceccadce40a3a66791bf1e8d0bdec
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-22 01:54:15 -0300

    ingame_hud: support for game over and victory labels

--- godot/scripts/ingame_hud.gd
+++ godot/scripts/ingame_hud.gd
@@ -2,9 +2,35 @@ extends CanvasLayer
 
 signal return_button_clicked
 
-func set_mine_count(count: int) -> void:
-	$Titlebar/MineCount.text = "Mines: %d" % count
+enum GameResult {
+	IN_PROGRESS,
+	GAME_OVER,
+	VICTORY
+}
 
+var result := GameResult.IN_PROGRESS
+
+func _ready() -> void:
+	update_result_view()
+
+func set_mine_count(count: int) -> void:
+	%MineCount.text = "Mines: %d" % count
+
+func update_result_view():
+	%GameOverLabel.visible = result == GameResult.GAME_OVER
+	%VictoryLabel.visible = result == GameResult.VICTORY
+
+func set_result_in_progress():
+	result = GameResult.IN_PROGRESS
+	update_result_view()
+
+func set_result_game_over():
+	result = GameResult.GAME_OVER
+	update_result_view()
+
+func set_result_victory():
+	result = GameResult.VICTORY
+	update_result_view()
 
 func _on_return_button_pressed() -> void:
 	return_button_clicked.emit()
