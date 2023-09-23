40603630a89f7b76d8d208035d6dc3faac341c3e
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-23 00:53:17 -0300

    refactor(scene): rename TileMap to GameGrid

--- godot/scripts/main.gd
+++ godot/scripts/main.gd
@@ -1,11 +1,9 @@
 extends Node
 
-@onready var tile_map := %TileMap
-
 var game_over := false
 
 func _ready() -> void:
-	hide_tile_map()
+	hide_game_grid()
 
 func start() -> void:
 	%StartMenuHUD.hide()
@@ -15,20 +13,20 @@ func start() -> void:
 	%IngameHUD.set_mine_count(mines)
 	%IngameHUD.set_result_in_progress()
 	%IngameHUD.show()
-	tile_map.init_board(width, height, mines)
+	%GameGrid.init_board(width, height, mines)
 	game_over = false
-	show_tile_map()
+	show_game_grid()
 
 ## Returns to the start menu.
 func go_back() -> void:
-	hide_tile_map()
+	hide_game_grid()
 	%StartMenuHUD.show_continue()
 	switch_to_start_menu()
 
 ## Resumes an ongoing game.
 func resume_game() -> void:
 	switch_to_game_hud()
-	show_tile_map()
+	show_game_grid()
 
 ## Hides the start menu and shows the in-game HUD.
 func switch_to_game_hud() -> void:
@@ -40,26 +38,26 @@ func switch_to_start_menu() -> void:
 	%IngameHUD.hide()
 	%StartMenuHUD.show()
 
-func show_tile_map() -> void:
-	%TileMapContainer.show()
-	%TileMap.game_active = not game_over
+func show_game_grid() -> void:
+	%GameGridContainer.show()
+	%GameGrid.game_active = not game_over
 
-func hide_tile_map() -> void:
-	%TileMapContainer.hide()
-	%TileMap.game_active = false
+func hide_game_grid() -> void:
+	%GameGridContainer.hide()
+	%GameGrid.game_active = false
 
 func end_game() -> void:
 	game_over = true
-	tile_map.game_active = false
+	%GameGrid.game_active = false
 
-func _on_tile_map_bomb_revealed() -> void:
-	tile_map.reveal_all_bombs()
+func _on_game_grid_bomb_revealed() -> void:
+	%GameGrid.reveal_all_bombs()
 	end_game()
 	%IngameHUD.set_result_game_over()
 
 func update_mines_left_count(new_mines_left_count: int) -> void:
 	%IngameHUD.set_mine_count(new_mines_left_count)
 
-func _on_tile_map_victory() -> void:
+func _on_game_grid_victory() -> void:
 	end_game()
 	%IngameHUD.set_result_victory()
