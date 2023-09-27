88daff51e01269fd41880ace7726e4d0af06b12e
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-27 03:07:48 -0300

    config-page: make theme color picker simpler

--- godot/scripts/config_page.gd
+++ godot/scripts/config_page.gd
@@ -3,5 +3,10 @@ extends Control
 ## Signals the user clicked "BACK" to return to the start menu.
 signal go_back_requested
 
+func _ready() -> void:
+	# disable 'swatches' and 'recent colors'
+	var picker: ColorPicker = %ThemePicker.get_picker()
+	picker.presets_visible = false
+
 func _on_return_button_pressed() -> void:
 	go_back_requested.emit()
