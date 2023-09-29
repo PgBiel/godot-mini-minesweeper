6c0151810da668bfca15f413ef8f673b60dd5928
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-29 01:39:33 -0300

    scripts/game_config: create generic Settings class

--- godot/scripts/game_config.gd
+++ godot/scripts/game_config.gd
@@ -3,40 +3,54 @@ extends Node
 
 signal appearance_bg_changed(new_value: Color)
 
-## A setting which is also a color.
-class ColorSetting:
+## A generic setting.
+class Setting:
 	## Setting value.
 	## Empty array means there is nothing.
-	var value: Array[Color]
+	var _value: Array
 
 	## Signal emitted for this setting.
 	var sgn: Signal
 
 	## Default value for this setting.
-	var default: Color
+	var _default
 
-	func _init(sgn: Signal, default: Color) -> void:
-		self.value = []
+	func _init(sgn: Signal, default) -> void:
+		self._value = []
 		self.sgn = sgn
-		self.default = default
+		self._default = default
 
-	func read() -> Color:
-		if value.is_empty():
-			return default
+	## Reads this setting's value.
+	func read():
+		if _value.is_empty():
+			return _default
 		else:
-			return value[0]
+			return _value[0]
 
-	func write(new_value: Color) -> void:
-		if value.is_empty():
-			value.append(new_value)
+	## Writes a new value for this setting.
+	func write(new_value) -> void:
+		if _value.is_empty():
+			_value.append(new_value)
 		else:
-			value[0] = new_value
+			_value[0] = new_value
 		sgn.emit(new_value)
 
+	## Resets this setting to its default.
 	func reset() -> void:
-		if not value.is_empty():
-			value.pop_front()
-			sgn.emit(default)
+		if not _value.is_empty():
+			_value.pop_front()
+			sgn.emit(_default)
+
+## A setting which is also a color.
+class ColorSetting extends Setting:
+	func _init(sgn: Signal, default: Color) -> void:
+		super(sgn, default)
+
+	func read() -> Color:
+		return super()
+
+	func write(new_value: Color) -> void:
+		super(new_value)
 
 ## The game's background setting.
 var appearance_bg := ColorSetting.new(appearance_bg_changed, Color("b5732d"))
